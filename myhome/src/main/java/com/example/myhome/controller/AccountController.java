83a51a77cb6656abd4b07be1755fad6cba86aaa7
hyesun123123 <goodnight4857@naver.com>
2021-01-21 22:41:37 +0900

    FetchType

--- myhome/src/main/java/com/example/myhome/controller/AccountController.java
+++ myhome/src/main/java/com/example/myhome/controller/AccountController.java
@@ -5,7 +5,6 @@ import com.example.myhome.model.User;
 import com.example.myhome.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
